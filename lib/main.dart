a595e713a89e42757594d038a1ec5309329f8cd4
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-09-10 12:52:03 +0530

    initialize the flutter binding

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot){
+       if(snapshot.connectionState == ConnectionState.waiting){
+         return const MaterialApp(
+           debugShowCheckedModeBanner: false,
+           home: Scaffold(
+             body: Center(
+               child: Center(
+                 child: Text("Welcome to Photo Sharing Clone App"),
+               ),
+             ),
+           ),
+         );
+       }
+       else if(snapshot.hasError){
+           return const MaterialApp(
+             debugShowCheckedModeBanner: false,
+             home: Scaffold(
+               body: Center(
+                 child: Center(
+                   child: Text("An Error  occurred, Please wait..."),
+                 ),
+               ),
+             ),
+           );
+         }
+       return const MaterialApp(
+         debugShowCheckedModeBanner: false,
+         title: "Flutter Photo Sharing Clone App",
+         home: Scaffold(),
+       );
+      }
     );
   }
 }
