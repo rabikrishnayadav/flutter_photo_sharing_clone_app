d00360a16efdf20f4f01d885fd24109c813beec4
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-09-11 20:54:43 +0530

    showing Owner data in owner Screen

--- lib/owner_details/owner_details.dart
+++ lib/owner_details/owner_details.dart
@@ -27,6 +27,68 @@ class OwnerDetails extends StatefulWidget {
 class _OwnerDetailsState extends State<OwnerDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.pink, Colors.deepOrange.shade300],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            stops: const [0.2, 0.9]
+          ),
+        ),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                Container(
+                  color: Colors.red,
+                  child: Column(
+                    children: [
+                      Image.network(
+                        widget.img!,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30.0,),
+                Text(
+                  'Owner Information',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.white54,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 30.0,),
+                Container(
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        widget.userImg!,
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30.0,),
+                Text(
+                  "Uploaded By : " + widget.name!,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
