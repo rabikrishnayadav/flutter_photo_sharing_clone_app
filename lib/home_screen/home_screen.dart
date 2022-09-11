922b75e787c703e14bdc0326d20d1758f3bb30e4
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-09-11 09:53:52 +0530

    floating Action button ui add on home screen

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -9,6 +9,57 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.pink, Colors.deepOrange.shade300],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: [0.2, 0.9],
+        ),
+      ),
+      child: Scaffold(
+        floatingActionButton: Wrap(
+          direction: Axis.horizontal,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              child: FloatingActionButton(
+                heroTag: "1",
+                backgroundColor: Colors.deepOrange.shade400,
+                onPressed: (){
+                  // show image Dialog
+                },
+                child: const Icon(Icons.camera_enhance),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              child: FloatingActionButton(
+                heroTag: "2",
+                backgroundColor: Colors.pink.shade400,
+                onPressed: (){
+                  // upload Image
+                },
+                child: const Icon(Icons.cloud_upload),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.deepOrange.shade300, Colors.pink],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                stops: const [0.2, 0.9],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
