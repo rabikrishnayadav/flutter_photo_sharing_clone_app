6a76857cf7c976f555f01a865a9622a22728aaab
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-09-11 09:23:13 +0530

    forget password ux add and its working fine

--- lib/forget_password/components/info.dart
+++ lib/forget_password/components/info.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_photo_sharing_clone_app/account_check/account_check.dart';
 import 'package:flutter_photo_sharing_clone_app/log_in/login_screen.dart';
+import 'package:flutter_photo_sharing_clone_app/sign_up/sign_up_screen.dart';
 import 'package:flutter_photo_sharing_clone_app/widgets/button_square.dart';
 import 'package:flutter_photo_sharing_clone_app/widgets/input_field.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Credentials extends StatelessWidget {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   final TextEditingController _emailTextController = TextEditingController(text: '');
 
@@ -36,9 +41,27 @@ class Credentials extends StatelessWidget {
             colors1: Colors.red,
             colors2: Colors.redAccent,
             press: () async{
-
+              try{
+                await _auth.sendPasswordResetEmail(email: _emailTextController.text);
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  backgroundColor: Colors.amber,
+                  content: Text("Password reset link has been sent to email address",
+                  style: TextStyle(fontSize: 12.0),),
+                ),
+                );
+              }
+              on FirebaseAuthException catch(error){
+                Fluttertoast.showToast(msg: error.toString());
+              }
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => LoginScreen()));
             },
             ),
+          TextButton(
+            onPressed: (){
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => SignUpScreen()));
+            },
+            child: const Center(child: Text("Create Account")),
+          ),
           AccountCheck(
               login: false,
               press: (){
