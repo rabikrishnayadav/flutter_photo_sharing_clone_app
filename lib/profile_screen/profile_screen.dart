63ad4d674f766423e5e05b84281ac5cb46cbd679
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-09-12 08:24:26 +0530

    appbar add on profile screen with back button

--- lib/profile_screen/profile_screen.dart
+++ lib/profile_screen/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_photo_sharing_clone_app/home_screen/home_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
 
@@ -9,6 +10,40 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.pink, Colors.deepOrange.shade300],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              stops: [0.2, 0.9],
+            ),
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.pink.shade400,
+        title: const Center(
+          child: Text("Profile Screen"),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white,),
+          onPressed: (){
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+          },
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepOrange.shade300, Colors.pink],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            stops: [0.2, 0.9],
+          ),
+        ),
+      ),
+    );
   }
 }
