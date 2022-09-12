83404e613075c9ebcd0b1ed0dd81a4b9aa421bec
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-09-12 09:09:58 +0530

    user data retrive from firestore to profile screen and back button added

--- lib/profile_screen/profile_screen.dart
+++ lib/profile_screen/profile_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_photo_sharing_clone_app/home_screen/home_screen.dart';
+import 'package:flutter_photo_sharing_clone_app/log_in/login_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
 
@@ -8,6 +13,36 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+
+  String? name = '';
+  String? email = '';
+  String? image = '';
+  String? phoneNo = '';
+
+  File? imageXFile;
+
+  Future _getDataFromDatabase() async{
+    await FirebaseFirestore.instance.collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get().then((snapshot) async {
+          if(snapshot.exists){
+            setState(() {
+              name = snapshot.data()!["name"];
+              email = snapshot.data()!["email"];
+              image = snapshot.data()!["userImage"];
+              phoneNo = snapshot.data()!["phoneNumber"];
+            });
+          }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getDataFromDatabase();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +77,75 @@ class _ProfileScreenState extends State<ProfileScreen> {
             end: Alignment.centerRight,
             stops: [0.2, 0.9],
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: (){
+                // show image dialog
+              },
+              child: CircleAvatar(
+                backgroundColor: Colors.amber,
+                minRadius: 85.0,
+                child: CircleAvatar(
+                  radius: 80.0,
+                  backgroundImage: imageXFile == null ? NetworkImage(image!) : Image.file(imageXFile!).image,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0,),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Name : ${name!}',
+                  style: const TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: (){
+                    // display text input dialog
+                  },
+                  icon: const Icon(Icons.edit),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10.0,),
+            Text(
+              'Email : ${email!}',
+              style: const TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 10.0,),
+            Text(
+              'Number : ${phoneNo!}',
+              style: const TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 10.0,),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => LoginScreen()));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              ),
+              child: const Text("Logout"),
+            ),
+          ],
         ),
       ),
     );
