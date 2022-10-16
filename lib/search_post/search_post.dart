e22e32adac72a99e81e14d2c4e85ae5eeab68f67
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-10-16 08:57:30 +0530

    AppBar Add on Search Post Screen

--- lib/search_post/search_post.dart
+++ lib/search_post/search_post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_photo_sharing_clone_app/home_screen/home_screen.dart';
 
 class SearchPost extends StatefulWidget {
 
@@ -9,6 +10,43 @@ class SearchPost extends StatefulWidget {
 class _SearchPostState extends State<SearchPost> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.deepOrange.shade300, Colors.pink,]
+              ),
+            ),
+          ),
+          title: TextField(
+            onChanged: (textEntered){
+
+            },
+            decoration: InputDecoration(
+              hintText: "Search Post here...",
+              hintStyle: const TextStyle(color: Colors.white54),
+              border: InputBorder.none,
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.search, color: Colors.white,),
+                onPressed: (){
+                  
+                },
+              ),
+              prefixIcon: IconButton(
+                  icon: const Padding(
+                      padding: EdgeInsets.only(right: 12.0, bottom: 4.0),
+                    child: Icon(Icons.arrow_back, color: Colors.white,),
+                  ),
+                onPressed: (){
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
